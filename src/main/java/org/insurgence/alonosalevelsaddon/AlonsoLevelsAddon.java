4b4ca331c64c0f290b315c8ab235a5897a9988ea
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-27 17:22:10 -0500

    api jar update

--- src/main/java/org/insurgence/alonosalevelsaddon/AlonsoLevelsAddon.java
+++ src/main/java/org/insurgence/alonosalevelsaddon/AlonsoLevelsAddon.java
@@ -1,16 +1,16 @@
 package org.insurgence.alonosalevelsaddon;
 
-import org.bukkit.Bukkit;
 import org.insurgence.alonosalevelsaddon.listeners.AlonsoLevelsEventListener;
 import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
+import org.insurgencedev.insurgenceboosters.libs.fo.Common;
 
-@IBoostersAddon(name = "AlonsoLevelsAddon", version = "1.0.0", author = "InsurgenceDev", description = "AlonsoLevels Support")
+@IBoostersAddon(name = "AlonsoLevelsAddon", version = "1.0.1", author = "InsurgenceDev", description = "AlonsoLevels Support")
 public class AlonsoLevelsAddon extends InsurgenceBoostersAddon {
 
     @Override
-    public void onAddonReloadablesStart() {
-        if (Bukkit.getPluginManager().isPluginEnabled("AlonsoLevels")) {
+    public void onAddonReloadAblesStart() {
+        if (Common.doesPluginExist("AlonsoLevels")) {
             registerEvent(new AlonsoLevelsEventListener());
         }
     }
